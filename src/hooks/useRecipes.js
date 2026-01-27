4074161272cfc1bc571ae48881b6fce2fe63e3da
marto55 <martin_my@abv.bg>
2026-01-27 21:29:42 +0200

    preventing the hook from firing if the search is empty

--- src/hooks/useRecipes.js
+++ src/hooks/useRecipes.js
@@ -1,14 +1,18 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-export const useRecipes = (searchTerm = 'chicken') => {
+export const useRecipes = (searchTerm = "chicken") => {
   return useQuery({
-    queryKey: ['recipes', searchTerm],
+    queryKey: ["recipes", searchTerm],
     queryFn: async () => {
       const { data } = await axios.get(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`,
       );
       return data.meals;
     },
+    // This prevents the API from firing if the search is empty
+    enabled: !!searchTerm,
   });
 };
+
+
