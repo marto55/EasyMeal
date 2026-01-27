ee22410e79c96cd3b38597686ecbc73296007090
marto55 <martin_my@abv.bg>
2026-01-27 21:32:54 +0200

    adding second hook for fetching recipe details

--- src/hooks/useRecipes.js
+++ src/hooks/useRecipes.js
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+// Hook to fetch recipes based on a search term
 export const useRecipes = (searchTerm = "chicken") => {
   return useQuery({
     queryKey: ["recipes", searchTerm],
@@ -15,4 +16,16 @@ export const useRecipes = (searchTerm = "chicken") => {
   });
 };
 
-
+// This is an additional hook to fetch details of a single recipe by ID
+export const useRecipeDetails = (id) => {
+  return useQuery({
+    queryKey: ['recipe', id],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+      );
+      return data.meals[0]; 
+    },
+    enabled: !!id,
+  });
+};
